1fc14943863686fd6948d1a2e633161b2303e593
BT_1N3_22 <BT_1N3_22@BT_1N3_22.bktech.local>
2017-12-15 14:52:49 -0500

    yes

--- OrcMathGUI/src/guiPlayer/Limit.java
+++ OrcMathGUI/src/guiPlayer/Limit.java
@@ -3,20 +3,23 @@ package guiPlayer;
 import java.awt.Color;
 import java.awt.Graphics2D;
 
+import guiTeacher.components.AnimatedComponent;
 import guiTeacher.components.Component;
 
-public class Limit extends Component {
+public class Limit extends AnimatedComponent {
 
 	public Limit() {
-		super(40, 40, 100, 100);
+		super(40, 40, 962, 10);
+		addSequence("resources/ken-sprite-sheet.png", 150, 0, 0, 60, 66, 2);
+		Thread animation = new Thread(this);
 		update();
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void update(Graphics2D g) {
-		g.setColor(Color.blue);
-		g.fillRect(0, 0, getWidth(), getHeight());
+		//g.setColor(Color.blue);
+		//g.fillRect(0, 0, getWidth(), getHeight());
+		super.update(g);
 	}
 
 }
