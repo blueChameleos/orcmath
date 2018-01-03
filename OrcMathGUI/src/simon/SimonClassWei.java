a67b98692710890715c1cb24371e7af138899b9a
BT_1N3_22 <BT_1N3_22@BT_1N3_22.bktech.local>
2018-01-03 15:34:40 -0500

    added simon package

---
+++ OrcMathGUI/src/simon/SimonClassWei.java
@@ -0,0 +1,23 @@
+package simon;
+
+import guiTeacher.GUIApplication;
+
+public class SimonClassWei extends GUIApplication {
+
+	public SimonClassWei(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initScreen() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
