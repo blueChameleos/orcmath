840618ecdd404ef258983fc83cdd522655c8526c
BT_1N3_24 <BT_1N3_24@BT_1N3_24.bktech.local>
2018-01-16 15:09:32 -0500

    Boss and Monster

---
+++ OrcMathGUI/src/maple_gacha/Boss.java
@@ -0,0 +1,10 @@
+package maple_gacha;
+
+public class Boss extends Monster implements Character {
+
+	public Boss(String img, int rank, int strength, int speed, int attack, int defense, int hp) {
+		super(img, rank, strength, speed, attack, defense, hp);
+		
+	}
+
+}
---
+++ OrcMathGUI/src/maple_gacha/Monster.java
@@ -0,0 +1,39 @@
+package maple_gacha;
+
+public class Monster extends Hero implements Character {
+
+	public Monster(String img, int rank, int strength, int speed, int attack, int defense, int hp) {
+		super(img, rank, strength, speed, attack, defense, hp);
+	}
+
+	@Override
+	public void attack() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getImage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getHP() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setHP() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void special() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
