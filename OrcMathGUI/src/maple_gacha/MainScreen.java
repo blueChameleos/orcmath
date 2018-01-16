071b0a701df8066144315a7fb23492eea93092e7
BT_1N3_21 <BT_1N3_21@BT_1N3_21.bktech.local>
2018-01-16 14:29:59 -0500

    created maple_gacha

---
+++ OrcMathGUI/src/maple_gacha/MainScreen.java
@@ -0,0 +1,26 @@
+package maple_gacha;
+
+import java.util.List;
+
+import guiTeacher.interfaces.Visible;
+import guiTeacher.userInterfaces.FullFunctionScreen;
+
+public class MainScreen extends FullFunctionScreen {
+
+	public MainScreen(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initAllObjects(List<Visible> viewObjects) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
