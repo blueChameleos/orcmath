086178846d41849a46ec15280b6671c2d657b968
BT_1N3_21 <BT_1N3_21@BT_1N3_21.bktech.local>
2018-01-16 14:47:50 -0500

    created gacha_battle branch

--- OrcMathGUI/src/maple_gacha/BattleScreen.java
+++ OrcMathGUI/src/maple_gacha/BattleScreen.java
@@ -2,10 +2,13 @@ package maple_gacha;
 
 import java.util.List;
 
+import guiTeacher.components.Graphic;
 import guiTeacher.interfaces.Visible;
 import guiTeacher.userInterfaces.FullFunctionScreen;
 
 public class BattleScreen extends FullFunctionScreen {
+	
+	private static final long serialVersionUID = 2809999782648181302L;
 
 	public BattleScreen(int width, int height) {
 		super(width, height);
@@ -14,7 +17,8 @@ public class BattleScreen extends FullFunctionScreen {
 
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) {
-
+		Graphic background = new Graphic(0, 0, getWidth(), getHeight(), "");
+		viewObjects.add(background);
 	}
 
 }
