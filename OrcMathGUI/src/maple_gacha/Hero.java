58d234fb6a8078f700cc4ad9e88d225d7510aa36
BT_1N3_23 <BT_1N3_23@BT_1N3_23.bktech.local>
2018-01-16 15:00:38 -0500

    Hero

---
+++ OrcMathGUI/src/maple_gacha/Hero.java
@@ -0,0 +1,23 @@
+package maple_gacha;
+
+public class Hero {
+
+	private String img;
+	private int rank;
+	private int strength;
+	private int speed;
+	private int attack;
+	private int defense;
+	private int hp;
+	
+	public Hero(String img, int rank, int strength, int speed, int attack, int defense, int hp) {
+		this.img = img;
+		this.rank = rank;
+		this.strength = strength;
+		this.speed = speed;
+		this.attack = attack;
+		this.defense = defense;
+		this.hp = hp;
+	}
+	
+}
