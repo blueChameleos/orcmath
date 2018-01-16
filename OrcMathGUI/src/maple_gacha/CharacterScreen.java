231c271aabdc9e0b924a2002285adaa89527e567
BT_1N3_24 <BT_1N3_24@BT_1N3_24.bktech.local>
2018-01-16 15:27:56 -0500

    screen

---
+++ OrcMathGUI/src/maple_gacha/CharacterScreen.java
@@ -0,0 +1,25 @@
+package maple_gacha;
+
+import java.util.List;
+
+import guiTeacher.components.Graphic;
+import guiTeacher.interfaces.Visible;
+import guiTeacher.userInterfaces.FullFunctionScreen;
+
+public class CharacterScreen extends FullFunctionScreen {
+	
+	
+	private static final long serialVersionUID = 6339468790728949736L;
+
+	public CharacterScreen(int width, int height) {
+		super(width, height);
+		setVisible(true);
+	}
+
+	@Override
+	public void initAllObjects(List<Visible> viewObjects) {
+		Graphic background = new Graphic(0, 0, getWidth(), getHeight(), "");
+		viewObjects.add(background);
+	}
+
+}
