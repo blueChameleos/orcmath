ab42e28d5c3f8c164170274e85b642a4b3a2db18
BT_1N3_16 <BT_1N3_16@BT_1N3_16.bktech.local>
2018-01-17 15:33:56 -0500

    Loading Screen almost done

--- OrcMathGUI/src/maple_gacha/LoadingScreen.java
+++ OrcMathGUI/src/maple_gacha/LoadingScreen.java
@@ -8,12 +8,20 @@ import java.io.File;
 
 import guiTeacher.components.Action;
 import guiTeacher.components.Button;
+import guiTeacher.components.Graphic;
+import guiTeacher.components.Link;
 import guiTeacher.components.TextArea;
 import guiTeacher.interfaces.Visible;
 import guiTeacher.userInterfaces.FullFunctionScreen;
+import holiday.HolidayCard;
+import holiday.Snowflake;
 
 public class LoadingScreen extends FullFunctionScreen{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private Button play;
 	private Button quit;
 	private TextArea name;
@@ -26,7 +34,7 @@ public class LoadingScreen extends FullFunctionScreen{
 
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) {
-		play = new Button(540,462,200,100,"PLAY",Color.green,new Action() {
+		play = new Link(540,462,200,100,"PLAY",new Action() {
 
 			@Override
 			public void act() {
@@ -35,15 +43,36 @@ public class LoadingScreen extends FullFunctionScreen{
 			}
 			
 		});
+		try {
+			File fontFile = new File("resources//Bobbleboddy.ttf");
+			// File fontFile = new File("resources//DayRoman.ttf");
+			Font font = Font.createFont(Font.TRUETYPE_FONT, fontFile);
+			Font baseFont=font.deriveFont(16f);
+			play.setFont(baseFont);
+			play.setSize(60);
+		} 
+		catch (Exception e) {
+			 e.printStackTrace();
+		}
 		quit = new Button(1000,900,200,50,"QUIT",Color.red,new Action() {
 
 			@Override
 			public void act() {
-				// TODO Auto-generated method stub
-				
+				System.exit(1);
 			}
 			
 		});
+		try {
+			File fontFile = new File("resources//Bobbleboddy.ttf");
+			// File fontFile = new File("resources//DayRoman.ttf");
+			Font font = Font.createFont(Font.TRUETYPE_FONT, fontFile);
+			Font baseFont=font.deriveFont(16f);
+			quit.setFont(baseFont);
+			quit.setSize(24);
+		} 
+		catch (Exception e) {
+			 e.printStackTrace();
+		}
 		name = new TextArea(420,200,600,200,"MapleStory");
 		try {
 			File fontFile = new File("resources//deloise.ttf");
@@ -55,10 +84,12 @@ public class LoadingScreen extends FullFunctionScreen{
 		} 
 		catch (Exception e) {
 			 e.printStackTrace();
-		}
+		}		
+		viewObjects.add(new Graphic(0, 0, getWidth(),getHeight(),"resources/homescreen.png"));
 		viewObjects.add(play);
 		viewObjects.add(quit);
-		viewObjects.add(name);
+		viewObjects.add(name);		
+
 	}
 
 	
