29491a9fa7e39850dc04fe6b6dc2105bdfae49aa
BT_1N3_24 <BT_1N3_24@BT_1N3_24.bktech.local>
2018-01-18 15:29:30 -0500

    d

--- OrcMathGUI/src/maple_gacha/CharacterScreen.java
+++ OrcMathGUI/src/maple_gacha/CharacterScreen.java
@@ -1,7 +1,11 @@
 package maple_gacha;
 
+
+import java.util.ArrayList;
 import java.util.List;
 
+import guiTeacher.components.Action;
+import guiTeacher.components.ClickableGraphic;
 import guiTeacher.components.Graphic;
 import guiTeacher.interfaces.Visible;
 import guiTeacher.userInterfaces.FullFunctionScreen;
@@ -10,6 +14,7 @@ public class CharacterScreen extends FullFunctionScreen {
 	
 	
 	private static final long serialVersionUID = 6339468790728949736L;
+	public static ArrayList<Hero> team;
 
 	public CharacterScreen(int width, int height) {
 		super(width, height);
@@ -18,8 +23,27 @@ public class CharacterScreen extends FullFunctionScreen {
 
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) {
-		Graphic background = new Graphic(0, 0, getWidth(), getHeight(), "");
+		Graphic background = new Graphic(0, 0, getWidth(), getHeight(), "resources/screenPics/cardSystem.png");
+		background.setVisible(true);
 		viewObjects.add(background);
-	}
+		team = new ArrayList<Hero>();
+		team.add(MainGame.game.beginnerArcher);
+		for(int i = 0; i < team.size(); i++) {
+				ClickableGraphic character = new ClickableGraphic(131,144,400,200, team.get(i).getImage());
+				character.setVisible(true);
+				character.setAction(new Action() {
 
+					@Override
+					public void act() {
+						character.setX(76);
+						character.setY(601);
+					}
+					
+				});
+				viewObjects.add(character);
+			}
+		
+	}
 }
+	
+	
