3902d19a928316848ab19e1bda2d3696560a4bdc
BT_1N3_21 <BT_1N3_21@BT_1N3_21.bktech.local>
2018-02-02 14:55:19 -0500

    in progression

--- OrcMathGUI/src/maple_gacha/NewBattleMenu.java
+++ OrcMathGUI/src/maple_gacha/NewBattleMenu.java
@@ -1,15 +1,26 @@
 package maple_gacha;
 
+import java.awt.Color;
 import java.util.ArrayList;
+import java.util.List;
 
+import guiPlayer.CustomPane;
+import guiTeacher.components.Action;
+import guiTeacher.components.Button;
 import guiTeacher.components.Pane;
+import guiTeacher.components.TextArea;
 import guiTeacher.interfaces.FocusController;
 import guiTeacher.interfaces.Visible;
 
 public class NewBattleMenu extends Pane {
 	
+	private static final long serialVersionUID = 6116383819049095100L;
 	private static final int WIDTH = 1000;
 	private static final int HEIGHT = 175;
+	public static Button attackbutton;
+	public static Button defbutton;
+	public static Button skillbutton;
+	public static Button itembutton;
 
 	public NewBattleMenu(FocusController focusController, int x, int y, int width, int height) {
 		super(focusController, x, y, WIDTH, HEIGHT);
@@ -22,4 +33,7 @@ public class NewBattleMenu extends Pane {
 		// TODO Auto-generated constructor stub
 	}
 
+	public void initAllObjects(List<Visible> viewObjects){
+		
+	}
 }
